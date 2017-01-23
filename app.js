4102e4b1246d7f83bda4096dbbf9fa9ed0774566
Miguel Pena <miguelp1986@gmail.com>
2017-01-23 12:33:53 -0800

    completed some functionality

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 
 var questionObjects = []; // this will hold our question objects
 
+var mainEl = document.getElementById('question-section');
+var divLeftEl, divCenterEl, divRightEl, imgLeftEl, imgCenterEl, imgRightEl, pLeftEl, pCenterEl, pRightEl;
+
 var questions = ['What level of difficulty are you looking for in a hike?', 'How do you feel about dogs and kids?', 'How much elevation gain are you willing to climb?', 'Which region do you prefer?', 'Which hiking movie did you like best?', 'If you HAD to choose, how would you rather die?', 'Do you require a spectacular view?', 'Are you looking for a hike with a lake, river, or waterfall?', 'Would you have cut off your arm? (ie; 127 hours, Aron Ralston)', ' Are you opposed to paying for parking?'];
 
 var textResponses = [['Taking a stroll across the Shire', 'Just to Rivendell and back', 'Taking the ring to Mount Doom'], ['Omg love them!', 'Neither here-nor-there', 'Ugh, is there a difference?'], ['Not much at all', 'A fair amount', 'I want the hike to take my breath away...literally'], ['Local (Washington State)', 'About an hour away', 'Literally as far away as possible'], ['Homeward Bound', 'Wild', 'Into Thin Air'], ['Of boredom ', 'Bears!', 'Avalanche'], ['I’m all about the journey', 'Yes! Do it for the gram!', 'I want to stand on the top of the world'], ['Yes', 'No', 'I’m looking not-die, how about that'], ['LOL NOPE', 'Depends (on which arm, do I have life insurance, are there camera crews nearby, etc)', 'Already have. I am Aron Ralston.'], ['Yes, morally', 'I’m willing to pay for parking, let’s get started!', 'Literally not worried about my car, man']];
@@ -15,11 +18,45 @@ function Question(question, textResponses, filePathResponses) {
 }
 
 function createImages() {
-  for (var i = 0; i < 2; i++) {
-    questionObjects.push(new Question(questions[i], textResponses[i], filePathResponses[i]));
+  for (var i = 0; i < 1; i++) {
+    questionObjects.push(new Question(questions[i], textResponses[i], filePathResponses));
   }
 }
 
-function displayQuestion(questionObject) {
-  
+function displayQuestion(questionObjects) {
+  for (var i = 0; i < 1; i++) {
+    divLeftEl = document.createElement('div');
+    divLeftEl.setAttribute('id', 'left');
+    imgLeftEl = document.createElement('img');
+    imgLeftEl.setAttribute('src', filePathResponses[0]);
+    pLeftEl = document.createElement('p');
+    pLeftEl.setAttribute('id', 'left');
+
+    divCenterEl = document.createElement('div');
+    divCenterEl.setAttribute('id', 'center');
+    imgCenterEl = document.createElement('img');
+    imgCenterEl.setAttribute('src', filePathResponses[1]);
+    pCenterEl = document.createElement('p');
+    pCenterEl.setAttribute('id', 'center');
+
+    divRightEl = document.createElement('div');
+    divRightEl.setAttribute('id', 'right');
+    imgRightEl = document.createElement('img');
+    imgRightEl.setAttribute('src', filePathResponses[2]);
+    pRightEl = document.createElement('p');
+    pRightEl.setAttribute('id', 'right');
+  }
+  divLeftEl.appendChild(imgLeftEl);
+  divLeftEl.appendChild(pLeftEl);
+  divCenterEl.appendChild(imgCenterEl);
+  divCenterEl.appendChild(pCenterEl);
+  divRightEl.appendChild(imgRightEl);
+  divRightEl.appendChild(pRightEl);
+
+  mainEl.appendChild(divLeftEl);
+  mainEl.appendChild(divCenterEl);
+  mainEl.appendChild(divRightEl);
 }
+
+createImages();
+displayQuestion(questionObjects[0]);
